e8082ae865029505073d4a4bba89d8cd0e19c69a
jayden-dang <jayden@jaydendang.com>
2024-08-04 20:26:35 +0700

    feat: initial docs

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Raki Finance</span>,
+  project: {
+    link: 'https://github.com/rakigeek',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/rakigeek',
+  footer: {
+    text: 'Raki Finance Documentation',
+  },
+}
+
+export default config
